6672cd2e9b162a4512b0fb605f1525af62213656
Peter Klingelhofer <peterklingelhofer@users.noreply.github.com>
2024-08-05 07:08:07 -0500

    Fix local storage SSR build

--- src/utils/setDarkMode.js
+++ src/utils/setDarkMode.js
@@ -1,27 +1,31 @@
 const setDarkMode = (theme) => {
-  const root = window.document.documentElement;
-  root.setAttribute("data-theme", theme);
-  localStorage.setItem("theme", theme);
+  if (typeof window !== "undefined") {
+    const root = window.document.documentElement;
+    root.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
 
-  // Apply class for theme
-  if (theme === "dark") {
-    root.classList.remove("light-mode");
-    root.classList.add("dark-mode");
-  } else {
-    root.classList.remove("dark-mode");
-    root.classList.add("light-mode");
+    if (theme === "dark") {
+      root.classList.remove("light-mode");
+      root.classList.add("dark-mode");
+    } else {
+      root.classList.remove("dark-mode");
+      root.classList.add("light-mode");
+    }
   }
 };
 
 export const initializeDarkMode = () => {
-  const localTheme = localStorage.getItem("theme");
-  const preferredTheme = window.matchMedia("(prefers-color-scheme: light)")
-    .matches
-    ? "light"
-    : "dark";
-  const theme = localTheme || preferredTheme;
-  setDarkMode(theme);
-  return theme;
+  if (typeof window !== "undefined" && typeof localStorage !== "undefined") {
+    const localTheme = localStorage.getItem("theme");
+    const preferredTheme = window.matchMedia("(prefers-color-scheme: light)")
+      .matches
+      ? "light"
+      : "dark";
+    const theme = localTheme || preferredTheme;
+    setDarkMode(theme);
+    return theme;
+  }
+  return "dark"; // Default to dark theme if not in the browser
 };
 
 export default setDarkMode;

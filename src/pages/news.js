874634a0bad36dfb64f5b4081afe7c9623f50aa8
Peter Klingelhofer <peterklingelhofer@users.noreply.github.com>
2025-01-20 20:51:53 -0600

    Fix left padding on RSS content

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,9 +1,12 @@
 // src/pages/blog.js
+// NOTE: Make this loosely match node_modules/gatsby-theme-carbon/src/templates/Default.js
+
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../gatsby-theme-carbon/components/Layout";
 import PageDescription from "gatsby-theme-carbon/src/components/PageDescription";
 import PageHeader from "gatsby-theme-carbon/src/components/PageHeader";
+import Main from "gatsby-theme-carbon/src/components/Main";
 
 const BlogPage = ({ data }) => {
   const posts = data.allFeedSubstackFeed.nodes;
@@ -15,41 +18,38 @@ const BlogPage = ({ data }) => {
       pageTitle="News"
       pageDescription="Check out our latest Substack posts."
       pageKeywords="newsletter, substack, posts"
-      children={<PageHeader title="News" label="label" theme="dark" />}
     >
-      <h1
-        id="page-title"
-        style={{ textAlign: "center", fontSize: 80, padding: "2rem" }}
-      >
-        News
-      </h1>
-      {posts.length === 0 && <p>No posts found.</p>}
-      {posts.map((post) => {
-        const date = new Date(post.pubDate).toLocaleDateString();
-        return (
-          <article
-            key={post.id}
-            style={{
-              maxWidth: "700px",
-              margin: "0 auto",
-              marginBottom: "2em",
-            }}
-          >
-            <h2>
-              <a href={post.link} target="_blank" rel="noopener noreferrer">
-                {post.title}
-              </a>
-            </h2>
-            <PageDescription>
-              <strong>Published on:</strong> {date}
-            </PageDescription>
-            <div
-              dangerouslySetInnerHTML={{ __html: post.content.encoded }}
-              style={{ marginTop: "1em" }}
-            />
-          </article>
-        );
-      })}
+      <PageHeader title="News" label="label" theme="dark" />
+
+      <Main padded>
+        {posts.length === 0 && <p>No posts found.</p>}
+        {posts.map((post) => {
+          const date = new Date(post.pubDate).toLocaleDateString();
+          return (
+            <article
+              key={post.id}
+              style={{
+                maxWidth: "700px",
+                margin: "0 auto",
+                marginBottom: "2em",
+              }}
+            >
+              <h2>
+                <a href={post.link} target="_blank" rel="noopener noreferrer">
+                  {post.title}
+                </a>
+              </h2>
+              <PageDescription>
+                <strong>Published on:</strong> {date}
+              </PageDescription>
+              <div
+                dangerouslySetInnerHTML={{ __html: post.content.encoded }}
+                style={{ marginTop: "1em" }}
+              />
+            </article>
+          );
+        })}
+      </Main>
     </Layout>
   );
 };

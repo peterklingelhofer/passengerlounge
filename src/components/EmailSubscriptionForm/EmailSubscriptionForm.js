295d83229882b7052136681ad1dfb246e1fbf37d
Peter Klingelhofer <peterklingelhofer@users.noreply.github.com>
2025-01-22 16:14:45 -0600

    Lazy load iframe

--- src/components/EmailSubscriptionForm/EmailSubscriptionForm.js
+++ src/components/EmailSubscriptionForm/EmailSubscriptionForm.js
@@ -5,18 +5,20 @@ export const EmailSubscriptionForm = ({ isLeftSideBar = false }) => {
   return (
     // <iframe
     //   src="https://cdn.forms-content-1.sg-form.com/d58ad9f4-b028-11ef-b98b-32170b0fc806"
+    //   loading="lazy"
+    //   title="Newsletter subscription form"
     //   style={{
     //     marginLeft: "auto",
     //     marginRight: "auto",
     //     minHeight: 442,
     //     width: "100%",
     //   }}
-    //   title="Newsletter subscription form"
     // />
     <div style={isLeftSideBar ? { marginTop: "50%" } : {}}>
       <iframe
         src="https://passengerlounge.substack.com/embed"
         title="Newsletter subscription form"
+        loading="lazy"
         style={{ width: "98%" }}
       />
     </div>
